7e96fd299579a6e95d655e31a66779fb2a3301f1
Joao Esperancinha <jofisaes@gmail.com>
2022-02-16 19:10:29 +0100

    🌿 Coverage badges

---
+++ matrix-anywhere-app/src/test/java/org/jesperancinha/matrixanywhere/MatrixActivityTest.java
@@ -0,0 +1,10 @@
+package org.jesperancinha.matrixanywhere;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixActivityTest {
+
+    @org.junit.jupiter.api.Test
+    void getDeterminant() {
+    }
+}
