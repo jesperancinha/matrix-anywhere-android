28179f1e1a50c4c16f109bcf3cf9df6330f91edf
Joao Esperancinha <jofisaes@gmail.com>
2023-08-21 18:04:48 +0200

    version 2.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.jesperancinha"
         minSdk = 33
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.0.2"
+        versionCode = 4
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
