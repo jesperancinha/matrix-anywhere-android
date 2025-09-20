137abbdc7aeccba2e767dab488a8a2ddc7e2ec38
João Esperancinha <jofisaes@gmail.com>
2025-09-20 16:52:07 +0200

    Adds compose version compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,11 +57,11 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("androidx.compose:compose-bom:2025.09.00"))
     implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
     implementation("androidx.activity:activity-compose:1.11.0")
-    implementation(platform("androidx.compose:compose-bom:2025.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
